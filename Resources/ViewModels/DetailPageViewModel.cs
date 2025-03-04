9f58f4fa3c19be8e77ca6be941eb7e83e3724adb
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-04 22:00:24 +0000

    Added DetailPage & TicketsPage VM

---
+++ Resources/ViewModels/DetailPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTSProject.Resources.ViewModels
+{
+    class DetailPageViewModel
+    {
+    }
+}
