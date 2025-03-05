64c27e0c071109991b46799c5ea6eed3bdafefa8
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-05 20:19:53 +0000

    Set binding to ConnectionsViewModel

--- Resources/Views/ConnectionsPage.xaml.cs
+++ Resources/Views/ConnectionsPage.xaml.cs
@@ -1,9 +1,12 @@
+using UTSProject.Resources.ViewModels;
+
 namespace UTSProject.Resources.Views;
 
 public partial class ConnectionsPage : ContentPage
 {
-	public ConnectionsPage()
+	public ConnectionsPage(ConnectionsViewModel vm)
 	{
 		InitializeComponent();
+		BindingContext = vm;
 	}
 }
