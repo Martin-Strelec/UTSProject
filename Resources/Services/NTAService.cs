9f6a7313024ab78a0cec0dc805247b6849535c4f
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-11 15:16:59 +0000

    Code cleanup

--- Resources/Services/NTAService.cs
+++ Resources/Services/NTAService.cs
@@ -1,38 +1,53 @@
-﻿using System.Net.Http;
+﻿using System.Diagnostics;
+using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using UTSProject.Resources.Models;
 
 namespace UTSProject.Resources.Services
 {
     public class NTAService
     {
-        private readonly HttpClient _httpClient;
-        private const string apiUrl = "https://api.nationaltransport.ie/gtfsr/v2/gtfsr?format=json"; //URI
-        private const string apiKey = "f94c4155f38c44cb86eee1acf9032f24"; //API key
+        HttpClient _client;
+        JsonSerializerOptions _serializerOptions;
+
+        private readonly string apiUri = "https://api.nationaltransport.ie/gtfsr/v2/gtfsr?format=json";
+        private readonly string apiKey = Environment.GetEnvironmentVariable("api_key");
+       
+        public List<Entity> Items { get; private set; }
+        public GtfsRealtimeResponse Response { get; private set; }
 
         public NTAService()
-        {
-            _httpClient = new HttpClient();
-            _httpClient.DefaultRequestHeaders.Add("x-api-key", apiKey);
+        { 
+            //Initializing HTTP Client
+          _client = new HttpClient();
         }
 
-        public async Task<List<Connection>> GetConnectionsAsync()
+        public async Task<List<Entity>> ReadDataAsync()
         {
-            try
-            {
-                var response = await _httpClient.GetAsync(apiUrl);
+            Response = new GtfsRealtimeResponse();
+
+            try { 
+            
+                //Request header
+                _client.DefaultRequestHeaders.Add("x-api-key", Environment.GetEnvironmentVariable("api_key"));
+
+                //Request method
+                HttpResponseMessage response = await _client.GetAsync(apiUri);
+
                 if (response.IsSuccessStatusCode)
                 {
-                    var json = await response.Content.ReadAsStringAsync();
-                    return JsonSerializer.Deserialize<List<Connection>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                    string content = await response.Content.ReadAsStringAsync();
+                    Response = JsonConvert.DeserializeObject<GtfsRealtimeResponse>(content);
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error fetching connections: {ex.Message}");
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
             }
-            return new List<Connection>();
+
+            return Response.Entity;
         }
     }
 }
