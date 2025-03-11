c97c9ea5307b66a98a2c4fc9a54c15ef73788c87
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-11 09:16:17 +0000

    Added UserInput model

---
+++ Resources/Models/UserInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTSProject.Resources.Models
+{
+    internal class UserInput
+    {
+        public DateTime Date {  get; set; }
+        public TimeSpan Time { get; set; }
+    }
+}
