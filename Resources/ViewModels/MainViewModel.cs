0745ad9fa653502c05b5682f4197de25a0b2a55a
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-11 15:18:58 +0000

    Added observable properties & Modified constructor & Added Search method

--- Resources/ViewModels/MainViewModel.cs
+++ Resources/ViewModels/MainViewModel.cs
@@ -1,13 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using UTSProject.Resources.Models;
+using UTSProject.Resources.Services;
+using UTSProject.Resources.Views;
 
 namespace UTSProject.Resources.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private DateTime _selectedDate;
+        [ObservableProperty]
+        private TimeSpan _selectedTime;
+
+        private readonly NTAService _publicTransportService;
+
+        public MainViewModel(NTAService publicTransportService)
+        {
+            _selectedDate = DateTime.Now;
+            _selectedTime = DateTime.Now.TimeOfDay;
+            _publicTransportService = publicTransportService;
+        }
+
+        [RelayCommand]
+        async Task Search()
+        {
+            UserInput input = new UserInput(SelectedDate, SelectedTime);
+
+            Dictionary<string, object> parameters = new Dictionary<string, object>()
+            {
+                {"userInput", input}
+            };
+
+            await Shell.Current.GoToAsync("ConnectionsPage", parameters);
+        }
     }
 }
