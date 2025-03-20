27bdb5327465b73c8b3e1317e2fa316e92df6938
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-20 09:10:26 +0000

    Added ConnectionDisplayModel

---
+++ Resources/Models/ConnectionDisplayData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTSProject.Resources.Models
+{
+    public class ConnectionDisplayData
+    {
+        public string Time {  get; set; }
+        public string Date { get; set; }
+        private string _vehicleID;
+        public string VehicleID
+        {
+            get => _vehicleID;
+            set
+            {
+                if (value == null)
+                {
+                    _vehicleID = "?";
+                }
+                else
+                {
+                    _vehicleID = value;
+                }
+
+            }
+        }
+    }
+}
