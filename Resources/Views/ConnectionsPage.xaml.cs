182ed171d0f65ab76f4746e0ad866939f162baee
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-25 21:38:11 +0000

    Dependency injection for ConnectionViewModel & Added OnPageLoaded method

--- Resources/Views/ConnectionsPage.xaml.cs
+++ Resources/Views/ConnectionsPage.xaml.cs
@@ -4,9 +4,16 @@ namespace UTSProject.Resources.Views;
 
 public partial class ConnectionsPage : ContentPage
 {
+    private ConnectionsViewModel _vm;
 	public ConnectionsPage(ConnectionsViewModel vm)
 	{
 		InitializeComponent();
+        _vm = vm;
 		BindingContext = vm;
 	}
+
+    private async void OnPageLoaded(object sender, EventArgs e)
+    {
+        _vm.LoadData();
+    }
 }
