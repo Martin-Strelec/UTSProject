6fbe2a063419201aaf68ee4f3a9d9cce4673f79e
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-25 17:31:48 +0000

    Added

---
+++ Resources/Models/StopModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTSProject.Resources.Models
+{
+    public class StopModel
+    {
+        [Column("stop_name")]
+        public string Name { get; set; }
+        [Column("stop_lat")]
+        public double Latitude { get; set; }
+        [Column("stop_lon")]
+        public double Longtitude { get; set; }
+        [Column("arrival_time")]
+        public TimeSpan ArrivalTime { get; set; }
+        [Column("departure_time")]
+        public TimeSpan DepartureTime { get; set; }
+    }
+}
---
+++ Resources/Services/DbService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Threading.Tasks;
+using UTSProject.Resources.Models;
+using System.Globalization;
+using System.Diagnostics;
+
+public class DbService
+{
+    private readonly string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=NTALocalDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    public async Task<List<StopModel>> GetTripStops(string tripID)
+    {
+        using (SqlConnection conn = new(_connectionString))
+        {
+            var stops = new List<StopModel>();
+            try
+            {
+                await conn.OpenAsync();
+                using (SqlCommand cmd = new("GetTripStops", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ETripID", tripID); // Add input parameter
+
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            stops.Add(new StopModel
+                            {
+                                Name = reader.GetString(1),
+                                Latitude = reader.GetDouble(2),
+                                Longtitude = reader.GetDouble(3),
+                                ArrivalTime = reader.GetTimeSpan(4),
+                                DepartureTime = reader.GetTimeSpan(5),
+                            });
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Database Error (GetRouteDetails): {e}");
+            }
+            return stops;
+        }
+    }
+}
