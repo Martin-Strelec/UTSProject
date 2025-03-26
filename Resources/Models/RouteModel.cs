9ffbdb6a59a7fe0dca1f0d2d5264e5ad2a7ad6e0
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-26 22:34:15 +0000

    Added RouteModel

---
+++ Resources/Models/RouteModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTSProject.Resources.Models
+{
+    public class RouteModel
+    {
+        [Column("route_long_name")]
+        public string RouteLongName {  get; set; }
+        [Column("route_short_name")]
+        public string RouteShortName { get; set; }
+        [Column("agency_name")]
+        public string AgencyName {  get; set; }
+        [Column("agency_url")]
+        public string AgencyURL { get; set; }
+    }
+}
