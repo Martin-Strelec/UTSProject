0bdea17571c0f934b2f39edb4900963621fb6b1a
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-26 22:34:50 +0000

    Added Observable properties for Route

--- Resources/ViewModels/ConnectionsViewModel.cs
+++ Resources/ViewModels/ConnectionsViewModel.cs
@@ -15,16 +15,16 @@ namespace UTSProject.Resources.ViewModels
         UserInput _userInput;
 
         [ObservableProperty]
-        private string _tripID;
-
-        [ObservableProperty]
         private string _date;
 
         [ObservableProperty]
         private string _name;
 
         [ObservableProperty]
-        private string _vehicleID;
+        private string _routeShortName;
+
+        [ObservableProperty]
+        private string _routeLongName;
 
         [ObservableProperty]
         private ObservableCollection<ConnectionDisplayData> _connections;
@@ -38,10 +38,12 @@ namespace UTSProject.Resources.ViewModels
         private bool _buttonIsVisible;
 
         private readonly NTAService _publicTransportService;
+        private readonly DbService _db;
 
-        public ConnectionsViewModel(NTAService publicTransportService)
+        public ConnectionsViewModel(NTAService publicTransportService, DbService db)
         {
             _publicTransportService = publicTransportService;
+            _db = db;
             ConnectionsIsVisible = false;
             ButtonIsVisible = false;
         }
@@ -53,17 +55,8 @@ namespace UTSProject.Resources.ViewModels
             Connections.Clear();
             // Hide the button
             ButtonIsVisible = false;
-            // Load new data
-            await foreach (var entity in _publicTransportService.InitializeData())
-            {
-                Connections.Add(new ConnectionDisplayData
-                {
-                    Date = entity.TripUpdate.Trip.StartDate,
-                    TripID = entity.TripUpdate.Trip.TripID,
-                    Time = entity.TripUpdate.Trip.StartTime,
-                    VehicleID = entity.TripUpdate.Vehicle?.Id
-                });
-            }
+            // Add data to ObservableColletion
+            AddToConnections();
             // Hide the button
             ButtonIsVisible = true;
         }
@@ -79,16 +72,9 @@ namespace UTSProject.Resources.ViewModels
             //Using NTAService method for storing entities
             await _publicTransportService.ReadDataAsync(UserInput.Time);
 
-            Debug.WriteLine(UserInput.Time);
-            await foreach (var entity in _publicTransportService.InitializeData())
-            {
-                Connections.Add(new ConnectionDisplayData
-                {
-                    Date = entity.TripUpdate.Trip.StartDate,
-                    Time = entity.TripUpdate.Trip.StartTime,
-                    VehicleID = entity.TripUpdate.Vehicle?.Id
-                });
-            }
+            //Debug.WriteLine(UserInput.Time);
+            // Add data to ObservableColletion
+            AddToConnections();
 
             //Hiding the loading indicator
             IndicatorIsVisible = false;
@@ -98,6 +84,53 @@ namespace UTSProject.Resources.ViewModels
 
             //Setting the Button to visible
             ButtonIsVisible = true;
+        }
+
+        private async void AddToConnections()
+        {
+            await foreach (var entity in _publicTransportService.InitializeData())
+            {
+                string tripID = entity.TripUpdate.Trip.TripID;
+
+                if (tripID == null)
+                {
+                    Debug.WriteLine("Skipping due to tripID being null");
+                    continue;
+                }
+
+                try
+                {
+                    var routeTask = _db.GetRouteDetails(tripID);
+                    var stopsTask = _db.GetTripStops(tripID);
+
+                    await Task.WhenAll(routeTask, stopsTask); // Run both tasks in parallel
+
+                    var dbRouteDetails = routeTask.Result;
+                    var dbStops = stopsTask.Result;
+
+                    if (dbRouteDetails == null || dbStops.Count == 0)
+                    {
+                        Debug.WriteLine($"Missing data for TripID: {entity.TripUpdate.Trip.TripID}");
+                        continue; // Skip this iteration
+                    }
+
+                    Connections.Add(new ConnectionDisplayData
+                    {
+                        Date = entity.TripUpdate.Trip.StartDate,
+                        Time = entity.TripUpdate.Trip.StartTime,
+                        RouteShortName = dbRouteDetails.RouteShortName,
+                        RouteLongName = dbRouteDetails.RouteLongName,
+                        StartPoint = dbStops[0].Name,
+                        EndPoint = dbStops[dbStops.Count - 1].Name
+                    });
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine($"Error with adding Connections: {e}");
+                }
+
+                
+            }
         }
     }
 }
