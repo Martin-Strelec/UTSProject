9cfbc8edb3eeaef3d9c564390fc564c94464f705
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-28 16:16:52 +0000

    Changed the layout & Added OnPageLoaded method to the code behind

--- Resources/Views/MainPage.xaml.cs
+++ Resources/Views/MainPage.xaml.cs
@@ -4,10 +4,19 @@ namespace UTSProject.Resources.Views;
 
 public partial class MainPage : ContentPage
 {
+	private MainViewModel _vm;
 	public MainPage(MainViewModel vm)
 	{
 		InitializeComponent();
+		_vm = vm;
 		BindingContext = vm;
 	}
+
+    private async void OnPageLoaded(object sender, EventArgs e)
+    {
+		_vm.ButtonIsVisible = true;
+		_vm.IndicatorIsRunning = false;
+    }
+
 
 }
