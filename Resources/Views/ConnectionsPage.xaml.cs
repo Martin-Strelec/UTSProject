80ac48a7a4dc9cff5050c84ef1ded672b4d44acd
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-28 16:15:53 +0000

    Added OnItemSelected method

--- Resources/Views/ConnectionsPage.xaml.cs
+++ Resources/Views/ConnectionsPage.xaml.cs
@@ -1,19 +1,27 @@
 using UTSProject.Resources.ViewModels;
+using UTSProject.Resources.Models;
 
 namespace UTSProject.Resources.Views;
 
 public partial class ConnectionsPage : ContentPage
 {
     private ConnectionsViewModel _vm;
-	public ConnectionsPage(ConnectionsViewModel vm)
-	{
-		InitializeComponent();
-        _vm = vm;
-		BindingContext = vm;
-	}
-
-    private async void OnPageLoaded(object sender, EventArgs e)
+    public ConnectionsPage(ConnectionsViewModel vm)
     {
-        _vm.LoadData();
+        InitializeComponent();
+        _vm = vm;
+        BindingContext = vm;
+    }
+
+    private async void OnItemSelected(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.FirstOrDefault() is ConnectionDetailsModel selectedItem)
+        {
+            var parameters = new Dictionary<string, object>
+        {
+            { "ConnectionDetails", selectedItem}
+        };
+            await Shell.Current.GoToAsync($"{nameof(ConnectionsPage)}/{nameof(DetailPage)}", parameters);
+        }
     }
 }
