63fef32fc347ee9adf51392a7a7a1b58e5d0adf8
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-28 22:39:10 +0000

    DI DetailViewModel

--- Resources/Views/DetailPage.xaml.cs
+++ Resources/Views/DetailPage.xaml.cs
@@ -1,12 +1,12 @@
 using UTSProject.Resources.ViewModels;
-
 namespace UTSProject.Resources.Views;
 
 public partial class DetailPage : ContentPage
 {
-	public DetailPage(ConnectionsViewModel vm)
-	{
-		InitializeComponent();
-		BindingContext = vm;
-	}
+
+    public DetailPage(DetailViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
 }
