ea212da24a08fd71ac2c0c40e10cfed9a3811862
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-28 16:12:47 +0000

    Added TripModel

---
+++ Resources/Models/TripModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTSProject.Resources.Models
+{
+    public class TripModel
+    {
+        [Column("trip_id")]
+        public string TripID { get; set; } 
+    }
+}
