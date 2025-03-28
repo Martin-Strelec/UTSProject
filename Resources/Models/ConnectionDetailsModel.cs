be10ac488f5bfa3bf72731a0e1850999ec0d41da
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-28 22:34:04 +0000

    Simplified properties

--- Resources/Models/ConnectionDetailsModel.cs
+++ Resources/Models/ConnectionDetailsModel.cs
@@ -8,29 +8,12 @@ namespace UTSProject.Resources.Models
 {
     public class ConnectionDetailsModel
     {
-        public string Time {  get; set; }
-        public string Date { get; set; }
+        public TimeSpan Time {  get; set; }
+        public DateTime Date { get; set; }
         public string RouteShortName { get; set; }
         public string RouteLongName { get; set; }
-        public StopModel StartPoint { get; set; }
-        public StopModel SearchedPoint { get; set; }
-        public StopModel EndPoint { get; set; }
-        public string EndPointArrival
-        {
-            get => EndPoint.ArrivalTime.ToString();
-        }
-        public string SearchedPointDeparture
-        {
-            get => SearchedPoint.DepartureTime.ToString();
-        }
-        public string EndPointName
-        {
-            get => EndPoint.Name.ToString();
-        }
-        public string SearchedPointName
-        {
-            get => SearchedPoint.Name.ToString();
-        }
+        public StopModel SearchedStop { get; set; }
+        public StopModel EndStop { get; set; }
         public List<StopModel> Stops { get; set; }
     }
 }
