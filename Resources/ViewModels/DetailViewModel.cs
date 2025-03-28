3b73c4a10c05c7441ba0d2062930a995c3d8abe3
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-28 22:37:52 +0000

    Added ConnectionDetails property & Added QueryProperty for passing data

--- Resources/ViewModels/DetailViewModel.cs
+++ Resources/ViewModels/DetailViewModel.cs
@@ -5,13 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using UTSProject.Resources.Models;
+using UTSProject.Resources.Services;
+using UTSProject.Resources.Views;
 
 namespace UTSProject.Resources.ViewModels
 {
-    [QueryProperty(nameof(ConnectionDetailsModel), "ConnectionDetails")]
-    public partial class DetailPageViewModel : ObservableObject
+    [QueryProperty("ConnectionDetails", "ConnectionDetails")]
+    public partial class DetailViewModel : ObservableObject
     {
         [ObservableProperty]
-        ConnectionDetailsModel _connectionDetails;
+        private ConnectionDetailsModel _connectionDetails;
     }
 }
