d733b001cbb19ccbe185b1f1203671bdbd93ee06
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-29 16:45:48 +0000

    Added SavedConnectionsService

---
+++ Resources/Services/SavedConnectionsService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UTSProject.Resources.Models;
+using UTSProject.Resources.ViewModels;
+
+namespace UTSProject.Resources.Services
+{
+    public class SavedConnectionsService
+    {
+        private List<ConnectionDetailsModel> _connections { get; set; }
+
+        public SavedConnectionsService()
+        {
+            _connections = new List<ConnectionDetailsModel>();
+        }
+
+        public void SaveConnection (ConnectionDetailsModel cd)
+        {
+            if (_connections != null)
+            {
+                _connections.Add(cd);
+            }
+        }
+        public List<ConnectionDetailsModel> GetConnections ()
+        {
+            return _connections;
+        }
+
+        public void RemoveFromConnections()
+        {
+        }
+    }
+}
