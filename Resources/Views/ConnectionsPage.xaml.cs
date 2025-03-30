9edd0acf981c97aa7cb5835f2ebefadae6848664
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-30 14:00:36 +0100

    DI LoadDataService and ConnectionsViewModel & Added ConnectionsPage_Loaded method

--- Resources/Views/ConnectionsPage.xaml.cs
+++ Resources/Views/ConnectionsPage.xaml.cs
@@ -1,25 +1,41 @@
 using UTSProject.Resources.ViewModels;
 using UTSProject.Resources.Models;
+using UTSProject.Resources.Services;
 
 namespace UTSProject.Resources.Views;
 
 public partial class ConnectionsPage : ContentPage
 {
-    public ConnectionsPage(ConnectionsViewModel vm)
+    private LoadDataService _ld;
+    private ConnectionsViewModel _vm;
+    public ConnectionsPage(ConnectionsViewModel vm, LoadDataService ld)
     {
         InitializeComponent();
         BindingContext = vm;
+        _vm = vm;
+        _ld = ld;
     }
 
     private async void OnItemSelected(object sender, SelectionChangedEventArgs e)
     {
         if (e.CurrentSelection.FirstOrDefault() is ConnectionDetailsModel selectedItem)
         {
-            await Shell.Current.GoToAsync($"{nameof(ConnectionsPage)}/{nameof(DetailPage)}",
+            await Shell.Current.GoToAsync($"/{nameof(ConnectionsPage)}/{nameof(DetailPage)}",
                 new Dictionary<string, object>
                 {
                     ["ConnectionDetails"] = selectedItem
                 });
         }
+    }
+
+    //private async void OnBackButtonClicked(object sender, EventArgs e)
+    //{
+    //    await Shell.Current.GoToAsync($"{nameof(MainPage)}"); // Manually navigate back
+    //}
+
+    private void ConnectionsPage_Loaded(object sender, EventArgs e)
+    {
+        ConnectionsList.SelectedItem = null;
+        _vm.Connections = _ld.Connections;
     }
 }
