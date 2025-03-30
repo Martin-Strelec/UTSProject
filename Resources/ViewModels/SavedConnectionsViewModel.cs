60f0a6a614e2960c8846752b96665ea92b24b7f2
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-30 13:59:03 +0100

    Added ViewModel

---
+++ Resources/ViewModels/SavedConnectionsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using UTSProject.Resources.Models;
+using UTSProject.Resources.Services;
+
+namespace UTSProject.Resources.ViewModels
+{
+    [QueryProperty("ConnectionDetails", "Connections")]
+    public partial class SavedConnectionsViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<ConnectionDetailsModel> _connections;
+    }
+}
