524164414983946009792be23688e8ac086769f7
Martin-Strelec <m.strelec2021@gmail.com>
2025-03-30 14:02:06 +0100

    Added DetailPage_Loaded and DeleteButton_Clicked method & Added statements to SaveButton_Clicked method

--- Resources/Views/DetailPage.xaml.cs
+++ Resources/Views/DetailPage.xaml.cs
@@ -15,13 +15,49 @@ public partial class DetailPage : ContentPage
 
     }
 
-    async void ImageButton_Clicked(object sender, EventArgs e)
+    private void DetailPage_Loaded(object sender, EventArgs e)
+    {
+        if (_vm.ConnectionDetails.IsNotSaved)
+        {
+            SaveButton.IsVisible = true;
+            DeleteButton.IsVisible = false;
+        }
+        else
+        {
+            SaveButton.IsVisible = false;
+            DeleteButton.IsVisible = true;
+        }
+    }
+
+    async void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        if (_vm.ConnectionDetails.IsNotSaved == false)
+        {
+            //Remove the connection from Saved connections
+            _sc.RemoveFromConnections(_vm.ConnectionDetails);
+            //Change the state of the connection to not saved
+            _vm.ConnectionDetails.IsNotSaved = true;
+            //Change the visibility of the buttons
+            DeleteButton.IsVisible = false;
+            SaveButton.IsVisible = true;
+            //Display alert
+            await DisplayAlert("Alert", "Connection has been deleted", "OK");
+        }
+        else
+        {
+            await DisplayAlert("Alert", "Cannot remove the saved item", "OK");
+        }
+    }
+
+    async void SaveButton_Clicked(object sender, EventArgs e)
     {
         //Check if the connection is saved already
         if (_vm.ConnectionDetails.IsNotSaved == true)
         {
             _sc.SaveConnection(_vm.ConnectionDetails);
             _vm.ConnectionDetails.IsNotSaved = false;
+            DeleteButton.IsVisible = true;
+            SaveButton.IsVisible = false;
             await DisplayAlert("Alert", "Connection has been saved!", "OK");
         }
         else
